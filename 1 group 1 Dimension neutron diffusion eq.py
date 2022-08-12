0f8b7a304c648bfa78b75c6b93f018a9624a803f
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2022-08-12 22:03:22 +0600

    Update 1 group 1 Dimension neutron diffusion eq.py

--- 1 group 1 Dimension neutron diffusion eq.py
+++ 1 group 1 Dimension neutron diffusion eq.py
@@ -3,26 +3,25 @@
 ####################################### but my initial guess is I used the wrong constants ############################################
 ################################# OK. the code is OK. The problem was I was using smaller mesh size and larger convergence value""""""
 """"""""""""""""""""""it should have been smaller mesh size and much smaller convergence value """""""""""""""""""""""""""""""""""
-
-
 import math 
 import numpy as np
 import time 
 import matplotlib.pyplot as plt 
 
-""" constant values """
-D_water=0.4
-D_fuel=0.35
 
-neu_sigma_f=1.5
-sigma_a_fuel=0.18
-sigma_a_water=0.003
-mesh_size=0.01
-external_source=10
+""" constant values """
+D_water=1.4
+D_fuel=.35
+
+neu_sigma_f=.24
+sigma_a_fuel=1.18
+sigma_a_water=0.3
+mesh_size=0.1
+external_source=100
 length=10
 total_mesh=int(length/mesh_size)
-
 """constants are loaded """
+
 
 
 """ necessary functions """
@@ -36,9 +35,6 @@ def constant_vector(size_of=0.1):
     constant_vector[int((size_of)*total_mesh):]= external_source*mesh_size
 
     return constant_vector
-
-
-
 
 
 def source_external(mesh_position,size_of=0.1 ):
@@ -89,15 +85,13 @@ def create_matrix():
         for j in range(1,total_mesh):
 
             if i==j:
-                
                 matrix[i][j]=diffusion_coefficient(i)/mesh_size+diffusion_coefficient(i+1)/mesh_size
                 +((absorbtion_cross_section(i)+absorbtion_cross_section(i+1)))*mesh_size/2
                 -(fission_crossection(i)+fission_crossection(i+1))*mesh_size/2
                 
                 matrix[i][j-1]=-diffusion_coefficient(i)/mesh_size
                 matrix[i][j+1]=-diffusion_coefficient(i+1)/mesh_size
-                
-    """ boundary conditons """
+    
     matrix[0][0]=diffusion_coefficient(1)/mesh_size+diffusion_coefficient(1+1)/mesh_size
     (absorbtion_cross_section(1)+absorbtion_cross_section(1+1))*mesh_size/2-(fission_crossection(1)+fission_crossection(1+1))*mesh_size/2
 
@@ -163,11 +157,12 @@ def solver (matrix,constant_vector,epsilon,initial_guess=None,over_relaxation_fa
 matrix=create_matrix()
 constant_vector=constant_vector()
 
-phi=solver(matrix,constant_vector,epsilon=0.1)
+phi=solver(matrix,constant_vector,epsilon=10**-3)
 
 x=np.arange(0,total_mesh+1)
 plt.figure(figsize=(10,6))
-plt.plot(x,phi,color="red")
+plt.plot(x,phi,color="green")
+plt.xlabel("mesh position")
+plt.ylabel("Neutron Flux")
 
 plt.show()
-
